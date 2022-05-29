1fe4f60665965243257aaf4018166bbd61a03395
Abdulmateen Tairu <tay2druh@gmail.com>
2022-05-29 03:57:06 +0100

    Document Create User endpoint

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -1,13 +1,19 @@
 import { UserTrackEnum } from '../enums/user-track.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ type: String })
   name: string;
 
+  @ApiProperty({ type: String })
   email: string;
 
+  @ApiProperty({ type: String, enum: UserTrackEnum })
   track: UserTrackEnum;
 
+  @ApiProperty({ type: Number })
   yearsOfExperience: number;
 
+  @ApiProperty({ type: [String] })
   tools: string[];
 }
